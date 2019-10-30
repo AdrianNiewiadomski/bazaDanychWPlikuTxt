38714e728e308866d046cbaa24acd747f6d6d275
AdrianNiewiadomski <kontakt@adrianniewiadomski.pl>
2019-10-30 17:23:52 +0100

    Odczytane wiersze umieszczono na stronie WWW w tabeli. Użyto funkcji explode()

--- index.php
+++ index.php
@@ -10,15 +10,33 @@
             $tablica = array();
 
             while( !feof($plik)){
-                // echo fgets($plik) . "<br/>";
-                $tablica[] = fgets($plik);
+                    // echo fgets($plik) . "<br/>";
+                    $tablica[] = fgets($plik);
             }
 
             fclose($plik);
-
-            foreach($tablica as $wiersz){
-                echo $wiersz . "<br/>";
-            }
         ?>
+        <table>
+			<tr>
+				<th>id</th>
+				<th>Imie</th>
+				<th>Nazwisko</th>
+				<th>Zawod</th>
+				<th>Pensja</th>
+			</tr>
+            <?php
+                foreach($tablica as $wiersz){
+                    // echo $wiersz . "<br/>";
+                    $rekord = explode(" ", $wiersz);
+					echo '<tr>';
+					echo "<td>$rekord[0]</td>";
+					echo "<td>$rekord[1]</td>";
+					echo "<td>$rekord[2]</td>";
+					echo "<td>$rekord[3]</td>";
+					echo "<td>$rekord[4]</td>";
+					echo "</tr>";
+                }
+            ?>
+        </table>
     </body>
 </html>
