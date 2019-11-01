b88dbda3c973345bb9444db61418503fd508b927
AdrianNiewiadomski <kontakt@adrianniewiadomski.pl>
2019-11-01 22:24:15 +0100

    Formularz został tak zmodyfikowany aby jego pola były automatycznie wypełniane wartościami odpowiednimi dla edytowanej osoby

--- formularz.php
+++ formularz.php
@@ -7,27 +7,48 @@
     </head>
     <body>
         <?php
+            $id =  "";
+            $imie =  "";
+            $nazwisko =  "";
+            $zawod =  "";
+            $pensja = "";
+
             if(isset($_GET['id'])){
-                echo $_GET['id'];
+                //echo $_GET['id'];
+                $id = $_GET['id'];
+
+                $plik = @fopen("plik.txt", "r") or die ("Błąd pliku!");
+                $i=1;
+                while( $i<$id){
+                        $tab = fgets($plik);
+                        $i++;
+                }
+                $wiersz = fgets($plik);
+                fclose($plik);
+                $rekord = explode(" ", $wiersz);
+                $imie = $rekord[1];
+                $nazwisko = $rekord[2];
+                $zawod = $rekord[3];
+                $pensja = $rekord[4];
             }
 
         ?>
 
         <form action="index.php" method="post">
             id:<br/>
-            <input type="text" name="id">
-            <br/>
-            Imię:<br/>
-            <input type="text" name="imie">
-            <br/>
-            Nazwisko:<br/>
-            <input type="text" name="nazwisko">
-            <br/>
-            Zawód:<br/>
-            <input type="text" name="zawod">
-            <br/>
-            Pensja:<br/>
-            <input type="text" name="pensja">
+
+            <?php
+                echo "<input type='text' name='id' value='$id'>";
+                echo "<br/>Imię:<br/>";
+                echo "<input type='text' name='imie' value='$imie'>";
+                echo "<br/>Nazwisko:<br/>";
+                echo "<input type='text' name='nazwisko' value='$nazwisko'>";
+                echo "<br/>Zawód:<br/>";
+                echo "<input type='text' name='zawod' value='$zawod'>";
+                echo "<br/>Pensja:<br/>";
+                echo "<input type='text' name='pensja' value='$pensja'>";
+            ?>
+
             <br/>
             <input type="submit" value="Zatwierdź">
             <a href="index.php">Anuluj</a>
